82b663b67fb6fa1ecf67df3c7622892115fe56b4
MohamadCS <mohamadessa443@gmail.com>
2025-07-10 00:03:28 +0300

    Fixed aerospace issue

--- include/FuzzyMac/NativeMacHandlers.hpp
+++ include/FuzzyMac/NativeMacHandlers.hpp
@@ -2,8 +2,10 @@
 
 #include <string>
 #include <vector>
+#include <QWidget>
 
 extern "C" void deactivateApp();
+extern "C" void makeWindowFloating(QWidget *widget);
 
 extern "C++" std::vector<std::string> spotlightSearch(const std::string& query,
                                                       const std::vector<std::string>& folders);
