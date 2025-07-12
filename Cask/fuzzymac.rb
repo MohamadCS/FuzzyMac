cffd4eed5b0910991de624eb33b4baffa57e8627
MohamadCS <mohamadessa443@gmail.com>
2025-07-12 18:07:12 +0300

    New cask formula

---
+++ Cask/fuzzymac.rb
@@ -0,0 +1,12 @@
+cask "fuzzymac" do
+  version "0.0.1"
+  sha256 "9a387ddfc29824f01e96da00c66542d5053ef5b261ca79b370efb15a8ebd03b6"
+
+  url "https://github.com/MohamadCS/FuzzyMac/releases/download/v0.0.1-alpha/FuzzyMac.zip"
+  name "FuzzyMac"
+  desc "GUI fuzzy finder for macOS"
+  homepage "https://github.com/MohamadCS/FuzzyMac"
+
+  app "FuzzyMac.app"
+
+end
