37da898ea9da3cae1c232bed6dfd6442bbfd156d
MohamadCS <mohamadessa443@gmail.com>
2025-07-23 00:47:41 +0300

    fixed cask typo

--- Casks/fuzzymac.rb
+++ Casks/fuzzymac.rb
@@ -1,8 +1,8 @@
 cask "fuzzymac" do
-  version "0.0.1"
+  version "0.0.2"
   sha256 "dbde002ecc7ca231d298d481bede9148ccf9e0e3802d4c71b2cb63f35953eb51"
 
-  url "https://github.com/MohamadCS/FuzzyMac/releases/download/v0.0.1-alpha/FuzzyMac.zip"
+  url "https://github.com/MohamadCS/FuzzyMac/releases/download/v0.0.2-alpha/FuzzyMac.zip"
   name "FuzzyMac"
   desc "GUI fuzzy finder for macOS"
   homepage "https://github.com/MohamadCS/FuzzyMac"
