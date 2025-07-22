be4e4a493fb0d7122a509173eba114cf9345a746
MohamadCS <mohamadessa443@gmail.com>
2025-07-23 00:45:15 +0300

    Updated cask

--- Casks/fuzzymac.rb
+++ Casks/fuzzymac.rb
@@ -1,6 +1,6 @@
 cask "fuzzymac" do
   version "0.0.1"
-  sha256 "566e99cf1f5e703f9485d2ec133c147666f6024bbcf0ea1b6ddd67f396c7972a"
+  sha256 "dbde002ecc7ca231d298d481bede9148ccf9e0e3802d4c71b2cb63f35953eb51"
 
   url "https://github.com/MohamadCS/FuzzyMac/releases/download/v0.0.1-alpha/FuzzyMac.zip"
   name "FuzzyMac"
